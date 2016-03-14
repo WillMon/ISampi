e3400fa0f50295b697d2dad1f54e8b9496efb3f6
optildeath <montero_w@live.com>
2016-03-14 07:57:54 -0500

    Init Commit
    Extra Practice Assignment given to me by my Sempi(Senor Class Men )

---
+++ ISampi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISampi
+{
+    interface IHighSchool
+    {
+        string School { get; set; }
+        int Grade { get; set; }
+
+        bool SenpaiNotice(IHighSchool senpai);
+
+    }
+    interface IMechPilot
+    {
+        string MechName { get; set; }
+        int Attack { get; set; }
+        int Defence { get; set; }
+
+        void ItsaAngle(IMonster angle);
+
+    }
+    interface IMonster
+    {
+        int Level { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
